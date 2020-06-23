c706c833921b848ac4c012e763d13ad8d8e8276e
jsondzj123 <duzhijian11@163.com>
2020-06-23 16:43:07 +0800

    1.修改课程表结构 2.增加公开课表结构

--- database/migrations/2020_06_23_162150_create_ld_course_open_table.php
+++ database/migrations/2020_06_23_162150_create_ld_course_open_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class CreateLdCourseTable extends Migration
+class CreateLdCourseOpenTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class CreateLdCourseTable extends Migration
      */
     public function up()
     {
-        Schema::create('ld_course', function (Blueprint $table) {
+        Schema::create('ld_course_open', function (Blueprint $table) {
             //字段设置部分
             $table->increments('id')->comment('自增id');
             $table->integer('admin_id')->default(0)->comment('操作员id');
@@ -24,16 +24,15 @@ class CreateLdCourseTable extends Migration
             $table->string('title' , 255)->default('')->comment('课程标题');
             $table->string('keywords' , 255)->default('')->comment('课程关键词');
             $table->string('cover' , 255)->default('')->comment('课程封面');
-            $table->decimal('pricing', 10, 2)->nullable()->comment('课程定价');
-            $table->decimal('sale_price', 10, 2)->nullable()->comment('优惠价格');
-            $table->integer('buy_num')->nullable()->default(0)->comment('购买基数');
-            $table->integer('expiry')->nullable()->default(0)->comment('课程有效期');
+            $table->integer('start_at')->nullable()->comment('公开课开始时间');
+            $table->integer('end_at')->nullable()->comment('公开课结束时间');
             $table->text('describe')->nullable()->comment('课程描述');
             $table->text('introduce')->nullable()->comment('课程介绍');
+            $table->tinyInteger('is_barrage')->default(0)->comment('是否弹幕(1代表是,0代表否)');
+            $table->tinyInteger('live_type')->default(6)->comment('直播类型(1语音云3大班5小班6大班互动)');
             $table->tinyInteger('nature')->default(0)->comment('课程属性(1代表授权,0代表自增)');
             $table->tinyInteger('status')->default(0)->comment('课程状态(0代表未发布,1代表在售(已发布),2代表停售(已下架))');
             $table->tinyInteger('is_del')->default(0)->comment('是否删除(1代表删除,0代表正常)');
-            $table->tinyInteger('is_recommend')->default(0)->comment('是否删除(1代表推荐,0代表不推荐)');
             $table->dateTime('create_at')->comment('创建时间');
             $table->dateTime('update_at')->nullable()->comment('更新时间');
 
@@ -46,7 +45,7 @@ class CreateLdCourseTable extends Migration
             $table->engine  = 'InnoDB';
         });
         //设置表注释
-        DB::statement("alter table `ld_course` comment '课程表'");
+        DB::statement("alter table `ld_course_open` comment '课程公开课表'");
     }
 
     /**
@@ -56,6 +55,6 @@ class CreateLdCourseTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ld_course');
+        Schema::dropIfExists('ld_course_open');
     }
 }
--- database/migrations/2020_06_23_162946_create_ld_course_open_teacher_table.php
+++ database/migrations/2020_06_23_162946_create_ld_course_open_teacher_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-class CreateLdCourseTeacherTable extends Migration
+class CreateLdCourseOpenTeacherTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,11 @@ class CreateLdCourseTeacherTable extends Migration
      */
     public function up()
     {
-        Schema::create('ld_course_teacher', function (Blueprint $table) {
+        Schema::create('ld_course_open_teacher', function (Blueprint $table) {
             //字段设置部分
             $table->increments('id')->comment('自增id');
             $table->integer('course_id')->default(0)->comment('课程id');
-            $table->integer('teacher_id')->default(0)->comment('讲师id');
+            $table->integer('teacher_id')->default(0)->comment('讲师或教务id');
             $table->tinyInteger('is_del')->default(0)->comment('是否删除(1代表删除,0代表正常)');
             $table->dateTime('create_at')->comment('创建时间');
             $table->dateTime('update_at')->nullable()->comment('更新时间');
@@ -31,7 +31,7 @@ class CreateLdCourseTeacherTable extends Migration
             $table->engine  = 'InnoDB';
         });
         //设置表注释
-        DB::statement("alter table `ld_course_teacher` comment '课程选择讲师表'");
+        DB::statement("alter table `ld_course_open_teacher` comment '公开课关联教师表'");
     }
 
     /**
@@ -41,6 +41,6 @@ class CreateLdCourseTeacherTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ld_course_teacher');
+        Schema::dropIfExists('ld_course_open_teacher');
     }
 }
