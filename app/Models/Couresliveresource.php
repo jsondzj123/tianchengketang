74fa9b0a3484fa909432550ffce8ebec9158c913
Suzhenwen <2663930382@qq.com>
2020-07-01 09:58:53 +0800

    添加课程关联直播资源model

---
+++ app/Models/Couresliveresource.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Couresliveresource extends Model {
+    //指定别的表名
+    public $table = 'ld_course_live_resource';
+    //时间戳设置
+    public $timestamps = false;
+}
