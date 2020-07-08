1400e384037e31b4e1886c1a1bf28f4bfee188cf
Suzhenwen <2663930382@qq.com>
2020-07-08 15:01:10 +0800

    课程增加缓存

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -392,10 +392,8 @@ class CourseController extends Controller {
                             $ziyuan = Video::where(['id'=>$val['resource_id'],'is_del'=>0,'status'=>0])->first()->toArray();
                             $val['ziyuan'] = $ziyuan;
                             //获取 学习时长
-                            echo $this->data['id'];
                             $MTCloud = new MTCloud();
                             $use_duration  =  $MTCloud->coursePlaybackVisitorList($this->data['id'],1,50)['data'];
-                            print_r($use_duration);die;
                             if(!empty($use_duration)){
                                 foreach ($use_duration as $kk=>$vv){
                                     if($vv['uid'] == $this->userid){
