d5bae6225e6a2b9af43ff0f2866620dac2af236e
Suzhenwen <2663930382@qq.com>
2020-07-09 17:55:43 +0800

    test

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -41,14 +41,7 @@ class UserController extends Controller {
         }
         return response()->json(['code' => 200 , 'msg' => '查询成功','data'=>$user]);
     }
-    //用户修改基本信息
-    public function userUpDetail(){
 
-    }
-    //用户修改联系方式
-    public function userUpRelation(){
-
-    }
     //用户更改手机号
     public function userUpPhone(){
         if(!isset($this->data['phone'])|| empty($this->data['phone'])){
@@ -63,7 +56,9 @@ class UserController extends Controller {
         }
         $up = Student::where(['id'=>$this->userid])->update(['phone'=>$this->data['phone']]);
         if($up){
-
+            return response()->json(['code' => 200 , 'msg' => '修改成功']);
+        }else{
+            return response()->json(['code' => 203 , 'msg' => '修改失败']);
         }
     }
     //用户更改邮箱
@@ -72,23 +67,65 @@ class UserController extends Controller {
             return response()->json(['code' => 201 , 'msg' => '邮箱地址不能为空']);
         }
         if (filter_var($this->data['email'], FILTER_VALIDATE_EMAIL)) {
-                $up = Student::where(['id'=>$this->userid])->update(['email'=>$this->data['email']]);
-                if($up){
-                    return response()->json(['code' => 200 , 'msg' => '修改成功']);
-                }else{
-                    return response()->json(['code' => 202 , 'msg' => '修改失败']);
-                }
+            $up = Student::where(['id'=>$this->userid])->update(['email'=>$this->data['email']]);
+            if($up){
+                return response()->json(['code' => 200 , 'msg' => '修改成功']);
+            }else{
+                return response()->json(['code' => 202 , 'msg' => '修改失败']);
+            }
         }else{
             return response()->json(['code' => 201 , 'msg' => '邮箱格式不正确']);
         }
     }
+
+    //用户修改基本信息
+    public function userUpDetail(){
+
+    }
+    //用户修改联系方式
+    public function userUpRelation(){
+
+    }
+
     //用户修改头像
     public function userUpImg(){
+        if(!isset($this->data['head_icon']) || empty($this->data['head_icon'])){
+            return response()->json(['code' => 201 , 'msg' => '头像为空']);
+        }
+        $up = Student::where(['id'=>$this->userid])->update(['user_icon'=>$this->data['head_icon']]);
+        if($up){
+            return response()->json(['code' => 200 , 'msg' => '修改成功']);
+        }else{
+            return response()->json(['code' => 203 , 'msg' => '修改失败']);
+        }
 
     }
     //用户修改密码
     public function userUpPass(){
-
+        if(!isset($this->data['old_pass']) || empty($this->data['old_pass'])){
+            return response()->json(['code' => 201 , 'msg' => '请输入旧密码']);
+        }
+        if(!isset($this->data['new_pass']) || empty($this->data['new_pass'])){
+            return response()->json(['code' => 201 , 'msg' => '请输入新密码']);
+        }
+        if(!isset($this->data['news_pass']) || empty($this->data['news_pass'])){
+            return response()->json(['code' => 201 , 'msg' => '请再次输入新密码']);
+        }
+        $user = Student::where(['id'=>$this->userid])->first()->toArray();
+        $olds_pass = password_hash($this->data['old_pass'] , PASSWORD_DEFAULT);
+        if($olds_pass != $user['password']){
+            return response()->json(['code' => 202 , 'msg' => '密码错误']);
+        }
+        if($this->data['new_pass'] != $this->data['news_pass']){
+            return response()->json(['code' => 202 , 'msg' => '两次输入不一致']);
+        }
+        $news_pass = password_hash($this->data['new_pass'] , PASSWORD_DEFAULT);
+        $up = Student::where(['id'=>$this->userid])->update(['pass'=>$news_pass]);
+        if($up){
+            return response()->json(['code' => 200 , 'msg' => '修改成功']);
+        }else{
+            return response()->json(['code' => 203 , 'msg' => '修改失败']);
+        }
     }
 
 
