5f5f6a833f8831f898b1581e5cf5a94001c83b8d
Suzhenwen <2663930382@qq.com>
2020-07-09 10:33:34 +0800

    课程

--- app/Http/Controllers/Web/PublicpayController.php
+++ app/Http/Controllers/Web/PublicpayController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\AdminLog;
+use App\Models\Coures;
 use App\Models\School;
 
 class PublicpayController extends Controller {
@@ -12,6 +13,9 @@ class PublicpayController extends Controller {
     protected $userid;
     public function __construct(){
         $this->data = $_REQUEST;
+        if(!isset($this->data['school_dns']) || empty($this->data['school_dns'])){
+            return response()->json(['code' => 201 , 'msg' => '请传域名']);
+        }
         $this->school = School::where(['dns'=>$this->data['school_dns']])->first();
         $this->userid = isset(AdminLog::getAdminInfo()->admin_user->id)?AdminLog::getAdminInfo()->admin_user->id:0;
     }
@@ -23,7 +27,7 @@ class PublicpayController extends Controller {
         */
    public function Transfer(){
        //根据学校查询支付信息
-       return ['code' => 200 , 'msg' => '查询成功','data'=>$this->school];
+       return response()->json(['code' => 200 , 'msg' => '查询成功','data'=>$this->school]);
    }
    /*
         * @param  OA流转订单
@@ -31,7 +35,6 @@ class PublicpayController extends Controller {
         * student_id  用户id
         * order_number 订单号
         * third_party_number 第三方支付订单号
-        * price  支付金额
         * pay_status 1定金2尾款3最后一笔款4全款
         * pay_type 1微信2支付宝3银行转账4汇聚5余额
         * pay_time 支付时间
@@ -41,7 +44,31 @@ class PublicpayController extends Controller {
         * return  array
         */
    public function orderOAtoPay(){
+       if(!isset($this->data['nature']) || empty($this->data['nature'])){
+           return response()->json(['code' => 201 , 'msg' => '课程类型不能为空']);
+       }
+       if(!isset($this->data['class_id']) || empty($this->data['class_id'])){
+           return response()->json(['code' => 201 , 'msg' => '课程不能为空']);
+       }
+       if(!isset($this->data['order_number']) || empty($this->data['order_number'])){
+           return response()->json(['code' => 201 , 'msg' => '订单号不能为空']);
+       }
+       if(!isset($this->data['pay_time']) || empty($this->data['pay_time'])){
+           return response()->json(['code' => 201 , 'msg' => '订单支付时间']);
+       }
+       if(!isset($this->data['pay_type']) || empty($this->data['pay_type'])){
+           return response()->json(['code' => 201 , 'msg' => '支付方式不能为空']);
+       }
+       if(!isset($this->data['pay_time']) || empty($this->data['pay_status'])){
+           return response()->json(['code' => 201 , 'msg' => '支付类型不能为空']);
+       }
+       if(!isset($this->data['student_id']) || empty($this->data['student_id'])){
+           return response()->json(['code' => 201 , 'msg' => '学生不能为空']);
+       }
        //查询课程信息
+       if($this->data['nature'] == 1){
+//            $course =
+       }
    }
 }
 
