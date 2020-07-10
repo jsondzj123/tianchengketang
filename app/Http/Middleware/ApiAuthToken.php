c985fe23dcc0950972411a9931435b37adba31b9
liagui <315142971@qq.com>
2020-07-11 03:12:18 +0800

    权限添加

--- app/Http/Middleware/ApiAuthToken.php
+++ app/Http/Middleware/ApiAuthToken.php
@@ -25,19 +25,28 @@ class ApiAuthToken {
             }
         } 
         $url = ltrim(parse_url($request->url(),PHP_URL_PATH),'/'); //获取路由连接
+        //print_r($url);die;
         $userlist = Admin::GetUserOne(['id'=>$user['id'],'is_forbid'=>1,'is_del'=>1]); //获取用户信息 
         if($userlist['code'] != 200){
             return response()->json(['code'=>403,'msg'=>'无此用户，请联系管理员']);
         }
         $authid = Authrules::getAuthOne($url);//获取权限id
-        if(!isset($authid['id'])||$authid['id'] <=0 ){
-            return response()->json(['code'=>403,'msg'=>'此用户没有权限']);
-        }
-        $role = Roleauth::getRoleOne($userlist['data']['role_id']);//获取角色权限
-        if(!strpos($role['data']['auth_id'],(string)$authid['id'])){
-           return response()->json(['code'=>403,'msg'=>'此用户没有权限！']);
+        
+        if(isset($authid['id'])&&$authid['id'] >0 ){
+
+            $role = Roleauth::getRoleOne($userlist['data']['role_id']);//获取角色权限
+           
+            if(!strpos($role['data']['auth_id'],(string)$authid['id'])){
+                
+               return response()->json(['code'=>403,'msg'=>'此用户没有权限！']);
+            }else{
+               
+               return $next($request);
+            }
         }else{
-           return $next($request);
+            return response()->json(['code'=>403,'msg'=>'此用户没有权限']);
+
         }
+
     }
 }
