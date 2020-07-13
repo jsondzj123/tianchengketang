0b115c68f3e75db210f613dd6c7a86903a9b7ca4
Suzhenwen <2663930382@qq.com>
2020-07-13 11:57:26 +0800

    test

--- app/Models/CouresSubject.php
+++ app/Models/CouresSubject.php
@@ -169,9 +169,16 @@ class CouresSubject extends Model {
             ->orderBydesc('id')->get()->toArray();
         //根据授权课程 获取分类
         $course = CourseSchool::select('parent_id')->where(['to_school_id'=>$school_id,'is_del'=>0])->get()->groupBy('parent_id');
-        print_r($course);
+        $lists=[];
+        if(!empty($course)){
+            foreach ($course as $k=>$v){
+                $two = self::select('id','parent_id','admin_id','school_id','subject_name as name','subject_cover as cover','subject_cover as cover','description','is_open','is_del','create_at')->where(['id'=>$v,'is_del'=>0])->first();
+                $lists = self::demo($two,0,0);
+            }
+        }
         $list = self::demo($one,0,0);
-        return ['code' => 200 , 'msg' => '获取成功','data'=>$list];
+        $listss = array_merge($list,$lists);
+        return ['code' => 200 , 'msg' => '获取成功','data'=>$listss];
     }
     //资源模块 条件显示
     public static function couresWheres(){
