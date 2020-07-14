79bcd9bd1e2c9496663d6aa59e271af0f9ba64b7
liagui <315142971@qq.com>
2020-07-14 12:11:02 +0800

    权限树调整

--- app/Models/AuthMap.php
+++ app/Models/AuthMap.php
@@ -15,7 +15,25 @@ class AuthMap extends Model {
     //时间戳设置
     public $timestamps = false;
 
-
+     /*
+         * @param  descriptsion 获取权限列表
+         * @param  $where[
+                    'id' => 权限id串
+                    ...
+                ]  查询条件
+         * @param  $field  字段
+         * @param  author  lys
+         * @param  ctime   2020/4/30
+         * return  array
+         */
+    public static function getAuthAlls($where=[],$field=['*']){
+        return  self::where(function($query) use ($where){ 
+            if(isset($where['id']) && $where['id'] != ''  ){
+                $query->whereIn('id',$where['id']);
+            }
+        })->select($field)->get()->toArray();        
+    }
+  
 
 
 }
