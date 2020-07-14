8ca732ffc55b575849f8640abf6b54ca72c1becc
Suzhenwen <2663930382@qq.com>
2020-07-14 10:42:07 +0800

    test

--- app/Models/Coureschapters.php
+++ app/Models/Coureschapters.php
@@ -12,6 +12,11 @@ class Coureschapters extends Model {
     /*=============章================*/
     //章节列表
     public static function chapterList($data){
+        $nature = isset($data['nature'])?$data['nature']:0;
+        if($nature == 1){
+            $course = CourseSchool::where(['id'=>$data['id']])->first();
+            $data['course_id'] = $course['course_id'];
+        }
         $lists = self::where(['course_id'=>$data['course_id'],'is_del'=>0,])->get()->toArray();
         $arr = self::demo($lists,0,0);
         return ['code' => 200 , 'msg' => '查询成功','data'=>$arr];
