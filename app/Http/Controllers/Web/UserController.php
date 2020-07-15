d0c089e87f8808e783e936c1064a28833a587dc8
Suzhenwen <2663930382@qq.com>
2020-07-15 11:17:19 +0800

    test

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -20,6 +20,7 @@ class UserController extends Controller {
     protected $userid;
     public function __construct(){
         $this->data = $_REQUEST;
+        print_r($this->data);die;
         if(!isset($this->data['school_dns']) || empty($this->data['school_dns'])){
             return response()->json(['code' => 201 , 'msg' => '请传分校域名']);
         }
