5d0d809b4fafd5347574d0e8fb584e663c910301
Suzhenwen <2663930382@qq.com>
2020-07-19 15:56:58 +0800

    test

--- app/Http/Controllers/Api/NotifyController.php
+++ app/Http/Controllers/Api/NotifyController.php
@@ -156,13 +156,13 @@ class NotifyController extends Controller {
             $html = $this->acurl($receiptData, 1);
             $arr = json_decode($html, true);
             //获取域名   正式环境 沙箱请求 不处理
-//            $url = $_SERVER["HTTP_REFERER"];
-//            if($url == 'tiancheng.longde999.cn'){
-//                $arr['pay_namess'] = "沙箱环境，不予处理金额变动";
-//                $arr['http_referer'] = $_SERVER["HTTP_REFERER"];
-//                file_put_contents('./orderpaylog/'.$order_number.'.txt', '时间:'.date('Y-m-d H:i:s').print_r($arr,true),FILE_APPEND);
-//                return response()->json(['code' => 200 , 'msg' =>'支付成功']);
-//            }
+            $url = $_SERVER["HTTP_REFERER"];
+            if($url == 'tiancheng.longde999.cn'){
+                $arr['pay_namess'] = "沙箱环境，不予处理金额变动";
+                $arr['http_referer'] = $_SERVER["HTTP_REFERER"];
+                file_put_contents('./orderpaylog/'.$order_number.'.txt', '时间:'.date('Y-m-d H:i:s').print_r($arr,true),FILE_APPEND);
+                return response()->json(['code' => 200 , 'msg' =>'支付成功']);
+            }
         }
         file_put_contents('./orderpaylog/'.$order_number.'.txt', '时间:'.date('Y-m-d H:i:s').print_r($arr,true),FILE_APPEND);
         // 判断是否购买成功  【状态码,0为成功（无论是沙箱环境还是正式环境只要数据正确status都会是：0）】
