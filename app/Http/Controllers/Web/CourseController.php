e30795ea6a086942037dea558749bbc2b2434ba9
Suzhenwen <2663930382@qq.com>
2020-07-20 15:04:34 +0800

    web test

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -56,7 +56,6 @@ class CourseController extends Controller {
                     }
                 }
             }
-            print_r($subject);die;
             //授权学科
             $course = CourseSchool::select('ld_course.parent_id')->leftJoin('ld_course','ld_course.id','=','ld_course_school.course_id')
                 ->where(['ld_course_school.to_school_id'=>$this->school['id'],'ld_course_school.is_del'=>0,'ld_course.is_del'=>0])->groupBy('ld_course.parent_id')->get()->toArray();
