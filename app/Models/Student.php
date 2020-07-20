c2cac66accbc59adbd946d47c728f91d003682d5
jsondzj123 <duzhijian11@163.com>
2020-07-20 11:10:14 +0800

    修改学员注册密码的问题

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -525,10 +525,14 @@ class Student extends Model {
         $school_id= isset(AdminLog::getAdminInfo()->admin_user->school_id) ? AdminLog::getAdminInfo()->admin_user->school_id : 0;
         $school_status = isset(AdminLog::getAdminInfo()->admin_user->school_status) ? AdminLog::getAdminInfo()->admin_user->school_status : 0;
         
+        //手机号后八位用于密码
+        $password = substr($body['phone'] , -8);
+        
         //组装学员数组信息
         $student_array = [
             'phone'         =>   $body['phone'] ,
-            'password'      =>   password_hash('123456' , PASSWORD_DEFAULT) ,
+            //'password'      =>   password_hash('12345678' , PASSWORD_DEFAULT) ,
+            'password'      =>   password_hash($password , PASSWORD_DEFAULT) ,
             'real_name'     =>   $body['real_name'] ,
             'sex'           =>   isset($body['sex']) && $body['sex'] == 1 ? 1 : 2 ,
             'papers_type'   =>   isset($body['papers_type']) && in_array($body['papers_type'] , [1,2,3,4,5,6,7]) ? $body['papers_type'] : 0 ,
