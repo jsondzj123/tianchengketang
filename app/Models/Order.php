21e3ea6ece95fc797a6d7aae4e0d40eb2ae71adf
Suzhenwen <2663930382@qq.com>
2020-07-20 16:38:50 +0800

    web test

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -439,7 +439,7 @@ class Order extends Model {
         if(empty($data['student_id'])){
             return ['code' => 201 , 'msg' => '学员id为空'];
         }
-        $order = self::select('*')->where(['student_id'=>$data['student_id']])->groupBy('class_id')->get()->toArray();
+        $order = self::where(['student_id'=>$data['student_id']])->get()->toArray();
         if(!empty($order)){
             foreach ($order as $k=>&$v){
                 if($v['pay_type'] == 0){
