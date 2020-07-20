e10083e6cb83c75b58326a2d1117fa49b35604e1
Suzhenwen <2663930382@qq.com>
2020-07-20 17:33:06 +0800

    web test

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -440,7 +440,7 @@ class Order extends Model {
             return ['code' => 201 , 'msg' => '学员id为空'];
         }
        // DB::enableQueryLog();
-        $order = self::selectRaw("any_value(class_id)")->where(['student_id'=>$data['student_id']])->groupBy('class_id')->get()->toArray();
+        $order = self::selectRaw("any_value(class_id)","pay_type" , "nature" , "status" , "pay_status")->where(['student_id'=>$data['student_id']])->groupBy('class_id')->get()->toArray();
 
         //$a = DB::getQueryLog();
         //return ['code' => 201 , 'msg' => $a];
