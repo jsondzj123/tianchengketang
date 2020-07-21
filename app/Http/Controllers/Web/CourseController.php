56c6d2d7fb4c35f32489916786e445e7634df366
Suzhenwen <2663930382@qq.com>
2020-07-21 14:09:59 +0800

    web test

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -533,52 +533,41 @@ class CourseController extends Controller {
 //                    'is_free' => 2
                 ];
             }
-            $key = "webCourseluboList" . $this->data['id'] . $this->userid;
-            if (Redis::get($key)) {
-                $recorde = json_decode(Redis::get($key), true);
-            } else {
-                //获取章
-                $recorde = Coureschapters::where(['course_id' => $this->data['id'], 'is_del' => 0, 'parent_id' => 0])->get()->toArray();
-                if (!empty($recorde)) {
-                    //循环章  查询每个章下的节
-                    foreach ($recorde as $k => &$v) {
-                        $recordes = Coureschapters::where(['course_id' => $this->data['id'], 'parent_id' => $v['id']])->where($chapterswhere)->get()->toArray();
-                        if (!empty($recordes)) {
-                            //循环每个小节 查询小节的进度
-                            foreach ($recordes as $key => &$val) {
-                                //查询小节绑定的录播资源
-                                $ziyuan = Video::where(['id' => $val['resource_id'], 'is_del' => 0, 'status' => 0])->first();
-                                $val['ziyuan'] = $ziyuan;
-                                if (empty($ziyuan)) {
-                                    $val['study'] = 0;
-                                } else {
-                                    $MTCloud = new MTCloud();
-                                    $use_duration = $MTCloud->coursePlaybackVisitorList($ziyuan['course_id'], 1, 50);
-                                    if (isset($use_duration['data']) || !empty($use_duration['data'])) {
-                                        foreach ($use_duration['data'] as $kk => $vv) {
-                                            if ($vv['uid'] == $this->userid) {
-                                                if ($vv['use_duration'] == 0) {
-                                                    $val['study'] = 0;
-                                                } else {
-                                                    $val['study'] = sprintf("%01.2f", $vv['use_duration'] / $vv['mt_duration'] * 100) . '%';
-                                                }
-                                            } else {
+            //获取章
+            $recorde = Coureschapters::where(['course_id' => $this->data['id'], 'is_del' => 0, 'parent_id' => 0])->get()->toArray();
+            if (!empty($recorde)) {
+                //循环章  查询每个章下的节
+                foreach ($recorde as $k => &$v) {
+                    $recordes = Coureschapters::where(['course_id' => $this->data['id'], 'parent_id' => $v['id']])->where($chapterswhere)->get()->toArray();
+                    if (!empty($recordes)) {
+                        //循环每个小节 查询小节的进度
+                        foreach ($recordes as $key => &$val) {
+                            //查询小节绑定的录播资源
+                            $ziyuan = Video::where(['id' => $val['resource_id'], 'is_del' => 0, 'status' => 0])->first();
+                            $val['ziyuan'] = $ziyuan;
+                            if (empty($ziyuan)) {
+                                $val['study'] = 0;
+                            } else {
+                                $MTCloud = new MTCloud();
+                                $use_duration = $MTCloud->coursePlaybackVisitorList($ziyuan['course_id'], 1, 50);
+                                if (isset($use_duration['data']) || !empty($use_duration['data'])) {
+                                    foreach ($use_duration['data'] as $kk => $vv) {
+                                        if ($vv['uid'] == $this->userid) {
+                                            if ($vv['use_duration'] == 0) {
                                                 $val['study'] = 0;
+                                            } else {
+                                                $val['study'] = sprintf("%01.2f", $vv['use_duration'] / $vv['mt_duration'] * 100) . '%';
                                             }
+                                        } else {
+                                            $val['study'] = 0;
                                         }
                                     }
                                 }
                             }
-                            $v['chapters'] = $recordes;
                         }
+                        $v['chapters'] = $recordes;
                     }
                 }
-                if($order > 0){
-                    $recorde['is_pay'] = 1;
-                }else{
-                    $recorde['is_pay'] = 0;
-                }
-                Redis::set($key,json_encode($recorde),300);
             }
         }
         return response()->json(['code' => 200 , 'msg' => '获取成功','data'=>$recorde]);
