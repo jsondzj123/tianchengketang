4f08e3ac2793cd577bdca2f4176d6a654db323bf
Suzhenwen <2663930382@qq.com>
2020-07-21 11:13:49 +0800

    web test

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -305,13 +305,13 @@ class UserController extends Controller {
                 }
                 //查讲师
                 $teacherlist = Couresteacher::where(['course_id'=>$courseid,'is_del'=>0])->get();
+                $string=[];
                 if(!empty($teacherlist)){
-//                    foreach ($teacherlist as $ks=>$vs){
-//                        $teacher = Teacher::where(['id'=>$vs['teacher_id'],'is_del'=>0,'type'=>2])->first();
-//                        $string[] = $teacher['real_name'];
-//                    }
-                    $course['teachername'] = '张三';
-//                    $course['teachername'] = implode(',',$string);
+                    foreach ($teacherlist as $ks=>$vs){
+                        $teacher = Teacher::where(['id'=>$vs['teacher_id'],'is_del'=>0,'type'=>2])->first();
+                        $string[] = $teacher['real_name'];
+                    }
+                    $course['teachername'] = implode(',',$string);
                 }
                 $courses[] = $course;
             }
