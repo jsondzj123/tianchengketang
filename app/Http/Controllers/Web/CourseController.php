70643e106eb97d819d4e73880baba897b3f88654
Suzhenwen <2663930382@qq.com>
2020-07-22 18:38:52 +0800

    web test

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -282,8 +282,8 @@ class CourseController extends Controller {
             $teacherlist = Couresteacher::where(['course_id' => $course['course_id'], 'is_del' => 0])->get();
             if (!empty($teacherlist)) {
                 foreach ($teacherlist as $k => $v) {
-                    $oneteacher = Teacher::where(['id' => $v['teacher_id'], 'is_del' => 0])->first();
-                    array_push($teacher, (array)$oneteacher);
+                    $oneteacher = Teacher::where(['id' => $v['teacher_id'], 'is_del' => 0])->first()->toArray();
+                    array_push($teacher, $oneteacher);
                 }
             }
             //收藏数量
@@ -298,9 +298,9 @@ class CourseController extends Controller {
             //修改观看数
             Coures::where(['id'=>$this->data['id']])->update(['watch_num'=>$course['watch_num']+1]);
             //授课方式
-            $method = Couresmethod::select('method_id')->where(['course_id' =>$this->data['id'],'is_del'=>0])->get();
+            $method = Couresmethod::select('method_id')->where(['course_id' =>$this->data['id'],'is_del'=>0])->get()->toArray();
             if (!empty($method)) {
-                $course['method'] = array_column((array)$method, 'method_id');
+                $course['method'] = array_column($method, 'method_id');
             }
             //学习人数   基数+订单数
             $ordernum = Order::where(['class_id' => $this->data['id'], 'status' => 2, 'oa_status' => 1,'nature'=>0])->count();
@@ -310,8 +310,8 @@ class CourseController extends Controller {
             $teacherlist = Couresteacher::where(['course_id' => $this->data['id'], 'is_del' => 0])->get();
             if (!empty($teacherlist)) {
                 foreach ($teacherlist as $k => $v) {
-                    $oneteacher = Teacher::where(['id' => $v['teacher_id'], 'is_del' => 0])->first();
-                    array_push($teacher, (array)$oneteacher);
+                    $oneteacher = Teacher::where(['id' => $v['teacher_id'], 'is_del' => 0])->first()->toArray();
+                    array_push($teacher, $oneteacher);
                 }
             }
             //收藏数量
