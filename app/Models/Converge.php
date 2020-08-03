52630de1bec1e2837f4d016096a16b1b2a03fd03
Suzhenwen <2663930382@qq.com>
2020-08-03 16:03:26 +0800

    支付

---
+++ app/Models/Converge.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Facades\DB;
+
+
+use App\Tools\CurrentAdmin;
+class Converge extends Model {
+    //指定别的表名
+    public $table = 'ld_converge';
+    //时间戳设置
+}
+
+
