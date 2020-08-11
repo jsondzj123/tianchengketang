1d4aa38415ed9288e4443e51f76f75646ca5b716
Suzhenwen <2663930382@qq.com>
2020-08-11 11:43:21 +0800

    订单

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -209,27 +209,27 @@ class OrderController extends Controller {
                 ->leftJoin('ld_student','ld_student.id','=','ld_order.student_id')
                 ->where(function($query) use ($data) {
                     if(isset($data['start_time']) && !empty($data['start_time'] != ''&&$data['start_time'] != 0 )){
-                        $query->where('ld_order.create_at','>=',$data['start_time']);
+                        $query->where('ld_order.create_at','>',$data['start_time']);
                     }
                     if(isset($data['end_time']) && !empty($data['end_time'] != ''&&$data['end_time'] != 0 )){
-                        $query->where('ld_order.create_at','<=',$data['end_time']);
+                        $query->where('ld_order.create_at','<',$data['end_time']);
                     }
                     $query->where('ld_order.status','=',1)
                         ->orwhere('ld_order.status','=',2);
                 })
                 ->get()->toArray();
-            foreach ($total as $k=>&$v){
-                if($v['nature'] == 1){
-                    $lesson = CourseSchool::where(['id'=>$v['class_id']])->first();
-                }else{
-                    $lesson = Coures::where(['id'=>$v['class_id']])->first();
-                }
-                $subject = Subject::where(['id'=>$lesson['parent_id']])->first();
-                $v['class_name'] = $lesson['title'];
-                $v['subject_name'] = $subject['subject_name'];
-                unset($v['class_id']);
-                unset($v['nature']);
-            }
+//            foreach ($total as $k=>&$v){
+//                if($v['nature'] == 1){
+//                    $lesson = CourseSchool::where(['id'=>$v['class_id']])->first();
+//                }else{
+//                    $lesson = Coures::where(['id'=>$v['class_id']])->first();
+//                }
+//                $subject = Subject::where(['id'=>$lesson['parent_id']])->first();
+//                $v['class_name'] = $lesson['title'];
+//                $v['subject_name'] = $subject['subject_name'];
+//                unset($v['class_id']);
+//                unset($v['nature']);
+//            }
             print_r($total);die;
 
 //        return Excel::download(new \App\Exports\FinanceExport(self::$accept_data), '财务报表.xlsx');
