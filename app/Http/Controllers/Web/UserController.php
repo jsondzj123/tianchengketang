944d8e0d0af0c29d0f0a3b362c9b3e792a852d4a
Suzhenwen <2663930382@qq.com>
2020-08-24 20:36:05 +0800

    前端显示

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -274,7 +274,6 @@ class UserController extends Controller {
     //我的课程
     public function myCourse(){
         $order = Order::where(['student_id'=>$this->userid,'status'=>2])
-            ->where('validity_time','>',date('Y-m-d H:i:s'))
             ->whereIn('pay_status',[3,4])
             ->get()->toArray();
         $courses = [];
