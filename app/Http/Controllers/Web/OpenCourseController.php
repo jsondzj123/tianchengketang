9f4c82d42458ef072c1dbbc04b25d32a224ba2ed
liagui <315142971@qq.com>
2020-08-26 15:58:12 +0800

    公开课观看数+1

--- app/Http/Controllers/Web/OpenCourseController.php
+++ app/Http/Controllers/Web/OpenCourseController.php
@@ -290,7 +290,7 @@ class OpenCourseController extends Controller {
         if(!isset($this->data['nickname'])  || empty($this->data['nickname'])){
             return response()->json(['code'=>201,'msg'=>'nickname为空或不合法']);
         }
-       
+        OpenLivesChilds::increment('watch_num',1);
         $openCourse = OpenLivesChilds::where(['lesson_id'=>$this->data['course_id'],'is_del'=>0,'is_forbid'=>0])->first();
         if(empty($openCourse)){
             return response()->json(['code'=>201,'msg'=>'非法请求！！！']);
