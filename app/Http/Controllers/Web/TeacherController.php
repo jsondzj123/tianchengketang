25df559997f9abfdd4740ab380c673a5f1b171f9
liagui <315142971@qq.com>
2020-08-28 17:47:26 +0800

    test

--- app/Http/Controllers/Web/TeacherController.php
+++ app/Http/Controllers/Web/TeacherController.php
@@ -133,7 +133,7 @@ class TeacherController extends Controller {
 			$data =Coures::leftJoin('ld_course_teacher','ld_course_teacher.course_id','=','ld_course.id')
 						->leftJoin('ld_lecturer_educationa','ld_lecturer_educationa.id','=','ld_course_teacher.teacher_id')
 						->where(['ld_course.is_del'=>0,'ld_course.school_id'=>$this->school['id'],'ld_course.status'=>1,'ld_lecturer_educationa.id'=>$this->data['teacher_id']])
-						->select('ld_course.cover','ld_course.title','ld_course.pricing','ld_course.buy_num','ld_lecturer_educationa.id','ld_course.id as course_id','ld_course_school.sale_price')
+						->select('ld_course.cover','ld_course.title','ld_course.pricing','ld_course.buy_num','ld_lecturer_educationa.id','ld_course.id as course_id','ld_course.sale_price')
 						->get()->toArray();
 			if(!empty($data)){
 				foreach ($data as $k => &$zizeng) {
