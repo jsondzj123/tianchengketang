a32455dca35db774a55cd26fe76a927025396e8a
jsondzj123 <duzhijian11@163.com>
2020-09-03 18:39:01 +0800

    增加试题难度字段

--- app/Http/Controllers/Web/BankController.php
+++ app/Http/Controllers/Web/BankController.php
@@ -1974,6 +1974,9 @@ class BankController extends Controller {
         //题型数组
         $exam_type_arr = [1=>'单选题',2=>'多选题',3=>'判断题',4=>'不定项',5=>'填空题',6=>'简答题'];
         
+        //试题难度数组
+        $exam_diffculty= [1=>'简单',2=>'一般',3=>'困难'];
+        
         //判断是否为章节练习
         if($type == 1){
             //判断章的id是否传递合法
@@ -2014,6 +2017,7 @@ class BankController extends Controller {
                     'exam_id'             =>  $v['exam_id'] ,
                     'exam_name'           =>  $exam_info['exam_content'] ,
                     'exam_type_name'      =>  $exam_type_name ,
+                    'exam_diffculty'      =>  isset($exam_diffculty[$exam_info['item_diffculty']]) ? $exam_diffculty[$exam_info['item_diffculty']] : '' ,
                     'text_analysis'       =>  $exam_info['text_analysis'] ,
                     'correct_answer'      =>  trim($exam_info['answer']) ,
                     'option_list'         =>  $option_content ,
@@ -2057,6 +2061,7 @@ class BankController extends Controller {
                     'exam_id'             =>  $v['exam_id'] ,
                     'exam_name'           =>  $exam_info['exam_content'] ,
                     'exam_type_name'      =>  $exam_type_name ,
+                    'exam_diffculty'      =>  isset($exam_diffculty[$exam_info['item_diffculty']]) ? $exam_diffculty[$exam_info['item_diffculty']] : '' ,
                     'text_analysis'       =>  $exam_info['text_analysis'] ,
                     'correct_answer'      =>  trim($exam_info['answer']) ,
                     'option_list'         =>  $option_content ,
@@ -2109,6 +2114,7 @@ class BankController extends Controller {
                     'exam_id'             =>  $v['exam_id'] ,
                     'exam_name'           =>  $exam_info['exam_content'] ,
                     'exam_type_name'      =>  $exam_type_name ,
+                    'exam_diffculty'      =>  isset($exam_diffculty[$exam_info['item_diffculty']]) ? $exam_diffculty[$exam_info['item_diffculty']] : '' ,
                     'text_analysis'       =>  $exam_info['text_analysis'] ,
                     'correct_answer'      =>  trim($exam_info['answer']) ,
                     'option_list'         =>  $option_content ,
