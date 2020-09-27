5456b0f206b738555dec78aae642ef49ebdd93b4
Suzhenwen <2663930382@qq.com>
2020-09-27 09:07:21 +0800

    讲师

--- app/Http/Controllers/Web/CourseController.php
+++ app/Http/Controllers/Web/CourseController.php
@@ -271,8 +271,10 @@ class CourseController extends Controller {
             $teacherlist = Couresteacher::where(['course_id' => $course['course_id'], 'is_del' => 0])->get();
             if (!empty($teacherlist)) {
                 foreach ($teacherlist as $k => $v) {
-                    $oneteacher = Teacher::where(['id' => $v['teacher_id'], 'is_del' => 0])->first();
-                    array_push($teacher, $oneteacher);
+                    if(!empty($v['teacher_id'])){
+                        $oneteacher = Teacher::where(['id' => $v['teacher_id'], 'is_del' => 0])->first()->toArray();
+                        array_push($teacher, $oneteacher);
+                    }
                 }
             }
             //收藏数量
