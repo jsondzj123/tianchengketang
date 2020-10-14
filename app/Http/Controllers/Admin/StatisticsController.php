3d0d79030ed16d667c56e9e17bff5d41e472d9fe
Suzhenwen <2663930382@qq.com>
2020-10-14 15:06:46 +0800

    教师统计

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -222,10 +222,11 @@ class StatisticsController extends Controller {
        return response()->json(['code'=>200,'msg'=>'获取成功','data'=>$studentList,'studentcount'=>$studentcount,'page'=>$page,'schoolList'=>$schoolList[0]]);
    }
    /*
-        * @param  课时统计
+        * @param  教师课时
         * @param  school_id  分校id
         * @param  real_name  讲师姓名
         * @param  phone  讲师手机号
+        * @param  time  查询类型1当天2昨天3七天4当月5三个月
         * @param  statr_time  开始时间
         * @param  end_time 结束时间
         * @param  author  苏振文
